19fc409146b38171124c0c8ffb664c9cad870314
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2024-06-04 14:09:33 -0400

    extract reusable base from index.html
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

--- razomy/function/server/app_lib.py
+++ razomy/function/server/app_lib.py
@@ -5,6 +5,9 @@ import os
 app = FastAPI()
 templates = Jinja2Templates(directory=f"{os.path.dirname(os.path.realpath(__file__))}/templates")
 
+from razomy.function.server import index_lib
+
+app.get("/")(index_lib.route_index)
 
 @app.get("/")
 async def read_root(request: Request):
